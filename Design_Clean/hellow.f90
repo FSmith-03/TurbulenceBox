6060b351053fcd3077c7ef5f0977f64c1c408cb7
FSmith-03 <fes45@cam.ac.uk>
2024-11-15 23:04:10 +0000

    hello

---
+++ Design_Clean/hellow.f90
@@ -0,0 +1,7 @@
+program hello
+    ! This is a comment line; it is ignored by the compiler
+    integer :: i
+    i = 122
+    print *, 'Hello, World!'
+    print *, 'The value of i is', i
+  end program hello
